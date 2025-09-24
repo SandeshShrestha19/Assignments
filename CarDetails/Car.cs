d710233921fcf67fa3630d2730c17e1bce140b22
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-09-24 11:46:17 +0545

    feat: add Program to demonstrate Car class functionality with multiple cars

---
+++ CarDetails/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDetails
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine($"{Model} made by {Make} is starting!");
+        }
+        public void Stop()
+        {
+            Console.WriteLine($"{Model} made by {Make} is stopping!");
+        }
+        public void Accelerate()
+        {
+            Console.WriteLine($"{Model} made by {Make} is accelerating!");
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"This is a {Color} colored {Model} made by {Make} company in {Year} AD.");
+        }
+    }
+}
---
+++ CarDetails/Program.cs
@@ -0,0 +1,26 @@
+﻿using CarDetails;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var cars = new List<Car>();
+        cars.Add(new Car {Make = "Toyota", Model = "Hilux", Year = 2019, Color = "Red"});
+        cars.Add(new Car {Make = "Buggati", Model = "Chiron", Year = 2015, Color = "Copper"});
+        cars.Add(new Car {Make = "Mercedes", Model = "S-Class", Year = 2022, Color = "Black"});
+        cars.Add(new Car {Make = "Rolls Royce", Model = "Phantom", Year = 2025, Color = "White"});
+
+        foreach (var car in cars)
+        {
+            car.Start();
+            car.Accelerate();
+            car.Stop();
+            car.GetInfo();
+
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+
+    }
+}
