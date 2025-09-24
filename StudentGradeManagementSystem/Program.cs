c64e6e1446327025ba496ad48d3690a33dfff122
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-09-24 10:37:55 +0545

    feat: add Student class with methods to collect grades, calculate average, and display reports

---
+++ StudentGradeManagementSystem/Program.cs
@@ -0,0 +1,21 @@
+﻿using StudentGradeManagementSystem;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var student = new Student();
+
+        student.GetGrades();
+
+        student.GetAverage();
+        
+        student.DisplayReports();
+
+        Console.ReadLine();
+    }
+}
+
+
+
---
+++ StudentGradeManagementSystem/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentGradeManagementSystem
+{
+    public class Student
+    {
+        public double[] Grades { get; set; }
+
+        /// <summary>
+        /// This method helps to collect the grades of multiple student and store those data in an array
+        /// </summary>
+        public void GetGrades()
+        {
+            Console.Write("How many grades do you want to enter? ");
+            var numberOfGrades = Convert.ToInt32(Console.ReadLine());
+
+            Grades = new double[numberOfGrades];
+
+            for (var i = 0; i < numberOfGrades; i++)
+            {
+                Console.Write($"Enter grade #{i + 1}: ");
+                Grades[i] = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+        /// <summary>
+        /// This method helps to calculate the average of the grades collected in an array
+        /// </summary>
+        public void GetAverage()
+        {
+            if (Grades == null || Grades.Length == 0)
+            {
+                Console.WriteLine("No grades were entered!");
+                return;
+            }
+
+            var averageGrades = Grades.Average();
+            Console.WriteLine($"The average of grades is {averageGrades:F2}.");
+
+            Console.WriteLine();
+        }
+        /// <summary>
+        /// This method displays the grades obtained by respective student.
+        /// </summary>
+        public void DisplayReports()
+        {
+            Console.WriteLine("|----Reports----|");
+
+            Console.WriteLine();
+
+            for (var i = 0;i < Grades.Length; i++)
+            {
+                Console.WriteLine($"Student {i+1} received grade: {Grades[i]:F2}.");
+            }
+        }
+    }
+}
+
