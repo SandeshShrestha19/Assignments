0d0e1964f8a75b4a348c940549bed3afb1b43cd8
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-10-08 13:08:05 +0545

    feat: implement simple log file writer to record messages

---
+++ LogFileWriter/LogFileWriter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogFileWriter
+{
+    public class LogFileWriter
+    {
+        private string LogFilePath;
+
+        public LogFileWriter(string logFilePath)
+        {
+            this.LogFilePath = logFilePath;
+        }
+        public void WriteLog(TypeOfLevel typeOfLevel, string message)
+        {
+            try
+            {
+                string logInput = $"{DateTime.UtcNow} [{typeOfLevel}] {message}";
+                File.AppendAllText(LogFilePath, $"{logInput}\n");
+            }
+            catch( Exception ex )
+            {
+                Console.WriteLine($"Failed to enter data: {ex.Message}");
+            }
+            
+        }
+    }
+}
---
+++ LogFileWriter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LogFileWriter;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var fileLogger = new LogFileWriter.LogFileWriter(logFilePath: @"C:\ROOT\BackupFolder\ThisIsATextFile.txt");
+        fileLogger.WriteLog(typeOfLevel: TypeOfLevel.INFO,message: "Application has started.");
+        fileLogger.WriteLog(typeOfLevel: TypeOfLevel.WARNING,message: "Updates might contain malware!");
+        fileLogger.WriteLog(typeOfLevel: TypeOfLevel.ERROR, message: "Error: Updates can't be implemented!");
+
+        Console.ReadLine();
+    }
+}
+
