d6d0868f9f1844356a90a24a4972cd67815435ed
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-10-28 11:29:54 +0545

    feat: add Transaction class with respective properties

--- SystemAssignment/Models/Transaction.cs
+++ SystemAssignment/Models/Transaction.cs
@@ -6,7 +6,7 @@ namespace SystemAssignment.Models
 {
     public class Transaction
     {
-        public int TransactionId { get; set; }
+        public int Id { get; set; }
         public TransactionType TypeOfTransaction { get; set; }
         public decimal Amount { get; set;}
         public DateTime DateTime { get; set; }
